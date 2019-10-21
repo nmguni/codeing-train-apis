a2c549cc4a43e523d68a9c2beecbf27535b15c56
Ndumiso Mguni <Ndumiso29@gmail.com>
2019-10-21 18:19:07 -0400

    part 1.2 working with api and data done

---
+++ app.js
@@ -0,0 +1,15 @@
+// goku image API
+      // async function catchGoku() {
+      //   const response = await fetch("goku.jpg"); 
+      //   const response2 = await fetch('goku2.jpg')
+      //   const blob = await response.blob();
+      //   document.getElementById("goku").src = URL.createObjectURL(blob);
+      // }
+
+      // catchGoku().then(response => {
+      //     console.log('yay')
+      // })
+      // .catch(error => {
+      //     console.log('WTFFFF!')
+      //     console.error(error)
+      // })
